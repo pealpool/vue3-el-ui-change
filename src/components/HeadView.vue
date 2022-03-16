431eb1f85536592926baae9e48a36de7033fa8f7
pealpool <pealpool@gmail.com>
2022-03-16 13:03:25 +0800

    assign

--- src/components/HeadView.vue
+++ src/components/HeadView.vue
@@ -1,11 +1,42 @@
 <template>
 
+  <div class="toolbar">
+    <el-dropdown>
+      <el-icon style="margin-right: 8px; margin-top: 1px"
+      >
+        <setting
+        />
+      </el-icon>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>View</el-dropdown-item>
+          <el-dropdown-item>Add</el-dropdown-item>
+          <el-dropdown-item>Delete</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+    <span>Tom</span>
+  </div>
+
 </template>
 
 <script lang='ts' setup>
+import {
+  Message, Menu as IconMenu, Setting, Location, Document,
+} from '@element-plus/icons-vue'
 
 </script>
 
 <style lang='scss' scoped>
+
+.toolbar {
+  position: absolute;
+  display: inline-flex;
+  align-items: center;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+}
+
 
 </style>
